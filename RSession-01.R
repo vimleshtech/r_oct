d6b0d120c059c724cadb7246ff7e6581ba1b4320
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2018-10-14 15:49:06 +0530

    r-1

---
+++ RSession-01.R
@@ -0,0 +1,51 @@
+
+# declare the variable , variable is temp. memory where data can be stored
+
+a <- 11 # here a is variable , and 11 is data or value 
+b <- 444
+c =a+b  #expression 
+print(a)  #output 
+
+
+##data type 
+a =11  #numeric / number
+b ="skhshsgh1123444&&%$$" #string /char
+
+#vector : collection of similar type data 
+sales <- c(1111,223333,44444,333333,2222)
+max(sales)
+min(sales)
+sum(sales)
+mean(sales)
+sd(sales)
+var(sales)
+
+
+##genderate the seriese 
+d <- c(1:100) #from 1 to 100
+
+sum(d*2)
+
+###
+d = read.csv("C:\\Users\\vkumar15\\Desktop\\backup\\Health data.csv")
+
+sum(d$PAYMENT)
+
+
+
+
+
+id<-c(1,2,3,4)
+d<-id*2
+
+barplot(id,d)
+
+name <- c("raman","jatin","divya","ayush")
+gender<-c("male","male","female","male")
+
+cbind(id,name,gender)
+
+
+
+
+
